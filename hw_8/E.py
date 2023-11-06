2ba23195950aba59b0dedc8b4c96a182a4d7aead
Tay Jen <helloevgenyy@gmail.com>
2023-11-06 14:58:25 +0300

    HW8 is done

--- hw_8/E.py
+++ hw_8/E.py
@@ -1,12 +1,23 @@
-def find_max(i, j):
-    if i >= j:
-        return 0
-    if (s[i] + s[j]) in ("()", "[]", "{}"):
-        return 2 + find_max(i + 1, j - 1)
-    if s[i] != s[j]:
-        return max(find_max(i+1, j), find_max(i, j-1))
+def longest_correct_bracket_subsequence(s: str):
+    n = len(s)
+
+    dp = []
+    for i in range(n):
+        dp.append([0] * n)
+
+    for i in range(n-1):
+        if (s[i] + s[i + 1]) in ("()", "[]", "{}"):
+            dp[i][i + 1] = 2
+
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if (s[i] + s[j]) in ("()", "[]", "{}"):
+                dp[i][j] = 2 + dp[i+1][j-1]
+            for k in range(i, j):
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k+1][j])
+
+    return dp[0][n-1]
 
 
-s = input()
-start, end = 0, len(s) - 1
-print(find_max(start, end))
+print(longest_correct_bracket_subsequence(input()))
