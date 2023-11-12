4bc6d0cd1da33b969ab1b7da25098e0983e9d95b
Tay Jen <helloevgenyy@gmail.com>
2023-11-12 22:58:37 +0300

    update, HW6 is done, working now

--- hw_6/F.py
+++ hw_6/F.py
@@ -1,14 +1,15 @@
 import sys
 
-N, K = [int(i) for i in input().split(' ')]
+N, K = map(int, input().split(' '))
 arr = [0] * (N + 1)
+t = [0] * (N + 1)
 
 
 def prefix_sum(i: int) -> int:
     res = 0
-    while i > 0:
-        res += arr[i]
-        i -= i & -i
+    while i >= 0:
+        res += t[i]
+        i = (i & (i + 1)) - 1
     return res
 
 
@@ -16,16 +17,17 @@ def range_sum(l: int, r: int) -> int:
     return prefix_sum(r) - prefix_sum(l-1)
 
 
-def update(i: int, x: int):
+def update(i: int, delta: int):
     while i <= N:
-        arr[i] += x
-        i += i & -i
+        t[i] += delta
+        i = i | (i + 1)
 
 
-for oper in sys.stdin.readlines():
-    if oper[0] == 'A':
-        i, x = map(int, oper.split(' ')[1:])
-        update(i, x)
-    elif oper[0] == 'Q':
-        l, r = map(int, oper.split(' ')[1:])
+for _ in range(K):
+    line = sys.stdin.readline()
+    if line[0] == 'A':
+        i, x = map(int, line.split(' ')[1:])
+        update(i, x - arr[i])
+    elif line[0] == 'Q':
+        l, r = map(int, line.split(' ')[1:])
         print(range_sum(l, r))
